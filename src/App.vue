6b021aaf7fb434d77c130ab6e5cf693cc1a3f778
saka-csys <sakamoto@csys.co.jp>
2020-10-10 10:09:27 +0900

    ToDoリストの追加、削除を１つのコンポーネントで実装

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div>
+    <h3>My Todo</h3>
+    <input v-model="newTodo" placeholder="Input here...">
+    <button v-on:click="addTodo()">ADD</button>
+
+    <h5>Todo List</h5>
+    <ul>
+      <li v-for="(todo,i) in todos" v-bind:key="i">
+        {{ todo }}
+        <button v-on:click="deleteTodo(i)">DEL</button>
+      </li>
+    </ul>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data() {
+    return {
+      todos: [],
+      newTodo: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo === '') return;
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    },
+    deleteTodo(i) {
+      this.todos.splice(i, 1);
+    }
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
