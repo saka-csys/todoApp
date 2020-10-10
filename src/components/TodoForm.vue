09a45417ab8b58ecb756c208d4c1c8b8168d408e
saka-csys <sakamoto@csys.co.jp>
2020-10-10 11:10:56 +0900

    複数コンポーネントで実装 App: Todoのデータを管理 TodoForm: Todoの追加 TodoList: リトス表示と削除

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>My Todo</h3>
+        <input v-model="newTodo" placeholder="Input here...">
+        <button v-on:click="addTodo()">ADD</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodo: ''
+        }
+    },
+    methods: {
+        addTodo() {
+            if (this.newTodo === '') return;
+            this.$emit('add', this.newTodo);
+            this.newTodo = '';
+        }
+    }
+}
+</script>
